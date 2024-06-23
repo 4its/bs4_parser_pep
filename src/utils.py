f05386235a9d488a7c86c02c012e43625bd56970
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-06-23 19:21:04 +0300

    fix logic to raise exceptions;  - at get_response;  - at find_tag;

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,7 @@
-import logging
-
 from bs4 import BeautifulSoup
 from requests import RequestException
 
-from constants import MESSAGES
+from constants import TEXTS
 from exceptions import ParserFindTagException
 
 
@@ -12,19 +10,16 @@ def get_response(session, url, encoding='utf-8'):
         response = session.get(url)
         response.encoding = encoding
         return response
-    except RequestException as e:
-        logging.exception(
-            MESSAGES.response_error.format(url, e),
-            stack_info=True,
+    except RequestException as error:
+        raise RequestException(
+            TEXTS.response_error.format(url, error)
         )
 
 
 def find_tag(soup, tag, attrs=None):
     searched_tag = soup.find(tag, attrs=(attrs or {}))
     if searched_tag is None:
-        error_message = MESSAGES.tag_not_found.format(tag, attrs)
-        logging.error(error_message, stack_info=True)
-        raise ParserFindTagException(error_message)
+        raise ParserFindTagException(TEXTS.tag_not_found.format(tag, attrs))
     return searched_tag
 
 
