803c77af0180e0995276636d6a84956a49bdd298
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-06-23 21:27:22 +0300

    rewrite control_output;

--- src/outputs.py
+++ src/outputs.py
@@ -7,18 +7,7 @@ from prettytable import PrettyTable
 from constants import BASE_DIR, CHOICES, DATETIME_FORMAT, DIRS, TEXTS
 
 
-def control_output(results, cli_args):
-    output_choices = {
-        CHOICES.pretty: pretty_output,
-        CHOICES.file: file_output,
-    }
-    if cli_args.output in output_choices:
-        output_choices[cli_args.output](results, cli_args)
-    else:
-        default_output(results)
-
-
-def default_output(results):
+def default_output(results, *args):
     for row in results:
         print(*row)
 
@@ -35,11 +24,20 @@ def file_output(results, cli_args):
     results_dir = BASE_DIR / DIRS.results
     results_dir.mkdir(exist_ok=True)
     parser_mode = cli_args.mode
-    now = dt.datetime.now()
-    now_formatted = now.strftime(DATETIME_FORMAT)
+    now_formatted = dt.datetime.now().strftime(DATETIME_FORMAT)
     file_name = f'{parser_mode}_{now_formatted}.csv'
     file_path = results_dir / file_name
     with open(file_path, 'w', encoding='utf-8') as file:
-        writer = csv.writer(file, dialect=csv.unix_dialect)
-        writer.writerows(results)
+        csv.writer(file, dialect=csv.unix_dialect).writerows(results)
     logging.info(TEXTS.file_result.format(file_path))
+
+
+OUTPUTS = {
+        CHOICES.pretty: pretty_output,
+        CHOICES.file: file_output,
+        None: default_output
+    }
+
+
+def control_output(results, cli_args):
+    OUTPUTS[cli_args.output](results, cli_args)
