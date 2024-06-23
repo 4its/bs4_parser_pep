0906a1a880ea6ab53f61406497c95399b7bbc264
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-06-23 15:53:10 +0300

    add constants; - CHOICES; - DIRS; - LOGS; - URLS; - TEXTS;

--- src/constants.py
+++ src/constants.py
@@ -1,24 +1,34 @@
 from pathlib import Path
+from urllib.parse import urljoin
 
+
+BASE_DIR = Path(__file__).parent
 MAIN_DOC_URL = 'https://docs.python.org/3/'
 MAIN_PEP_URL = 'https://peps.python.org/'
 
-BASE_DIR = Path(__file__).parent
 
-DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
-EXPECTED_STATUS = {
-    'A': ('Active', 'Accepted'),
-    'D': ('Deferred',),
-    'F': ('Final',),
-    'P': ('Provisional',),
-    'R': ('Rejected',),
-    'S': ('Superseded',),
-    'W': ('Withdrawn',),
-    '': ('Draft', 'Active'),
-}
+class CHOICES:
+    pretty = 'pretty'
+    file = 'file'
 
 
-class MESSAGES:
+class DIRS:
+    downloads = 'downloads'
+    results = 'results'
+
+
+class LOGS:
+    directory = BASE_DIR / 'logs'
+    filename = 'parser.log'
+    file_path = directory / filename
+
+
+class URLS:
+    download = urljoin(MAIN_DOC_URL, 'download.html')
+    whats_new = urljoin(MAIN_DOC_URL, 'whatsnew/')
+
+
+class TEXTS:
     start_parse = 'Парсер запущен!'
     finish_parse = 'Парсер завершил работу.'
     command_args = 'Аргументы командной строки: {}'
@@ -34,3 +44,18 @@ class MESSAGES:
     )
     tqdm_description = 'Парсим список PEP'
     load_archive = 'Архив был загружен и сохранён: {}'
+
+
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+EXPECTED_STATUS = {
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
+}
+
+
