3f0f339fe90c9e049b99584b5df2deb0564aa6fb
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-06-23 15:54:01 +0300

    fix configs.py to use constants;  - LOGS;  - TEXTS;

--- src/configs.py
+++ src/configs.py
@@ -2,40 +2,38 @@ import argparse
 import logging
 from logging.handlers import RotatingFileHandler
 
-from constants import BASE_DIR, MESSAGES
+from constants import LOGS, TEXTS
 
 LOG_FORMAT = '%(asctime)s - [%(levelname)s] - %(message)s'
 DT_FORMAT = '%d.%m.%Y %H:%M:%S'
 
 
 def configure_argument_parser(available_modes):
-    parser = argparse.ArgumentParser(description=MESSAGES.parser_description)
+    parser = argparse.ArgumentParser(description=TEXTS.parser_description)
     parser.add_argument(
         'mode',
         choices=available_modes,
-        help=MESSAGES.parser_mode
+        help=TEXTS.parser_mode
     )
     parser.add_argument(
         '-c',
         '--clear-cache',
         action='store_true',
-        help=MESSAGES.parser_cache_clean
+        help=TEXTS.parser_cache_clean
     )
     parser.add_argument(
         '-o',
         '--output',
         choices=('pretty', 'file'),
-        help=MESSAGES.parser_outputs
+        help=TEXTS.parser_outputs
     )
     return parser
 
 
 def configure_logging():
-    log_dir = BASE_DIR / 'logs'
-    log_dir.mkdir(exist_ok=True)
-    log_file = log_dir / 'parser.log'
+    LOGS.directory.mkdir(exist_ok=True)
     rotating_handler = RotatingFileHandler(
-        log_file, maxBytes=10 ** 6, backupCount=5
+        LOGS.file_path, maxBytes=10 ** 6, backupCount=5
     )
     logging.basicConfig(
         datefmt=DT_FORMAT,
