9cd466d915665db9a0681e467a989e0d293a3d7d
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-06-23 15:55:25 +0300

    fix outputs.py to use constants;  - CHOICES;  - DIRS;  - TEXTS;  - fix logic in choice_output func;

--- src/outputs.py
+++ src/outputs.py
@@ -4,15 +4,16 @@ import logging
 
 from prettytable import PrettyTable
 
-from constants import BASE_DIR, DATETIME_FORMAT, MESSAGES
+from constants import BASE_DIR, CHOICES, DATETIME_FORMAT, DIRS, TEXTS
 
 
 def control_output(results, cli_args):
-    output = cli_args.output
-    if output == 'pretty':
-        pretty_output(results)
-    elif output == 'file':
-        file_output(results, cli_args)
+    output_choices = {
+        CHOICES.pretty: pretty_output,
+        CHOICES.file: file_output,
+    }
+    if cli_args.output in output_choices:
+        output_choices[cli_args.output](results, cli_args)
     else:
         default_output(results)
 
@@ -22,7 +23,7 @@ def default_output(results):
         print(*row)
 
 
-def pretty_output(results):
+def pretty_output(results, *args):
     table = PrettyTable()
     table.field_names = results[0]
     table.align = 'l'
@@ -31,7 +32,7 @@ def pretty_output(results):
 
 
 def file_output(results, cli_args):
-    results_dir = BASE_DIR / 'results'
+    results_dir = BASE_DIR / DIRS.results
     results_dir.mkdir(exist_ok=True)
     parser_mode = cli_args.mode
     now = dt.datetime.now()
@@ -41,4 +42,4 @@ def file_output(results, cli_args):
     with open(file_path, 'w', encoding='utf-8') as file:
         writer = csv.writer(file, dialect=csv.unix_dialect)
         writer.writerows(results)
-    logging.info(MESSAGES.file_result.format(file_path))
+    logging.info(TEXTS.file_result.format(file_path))
